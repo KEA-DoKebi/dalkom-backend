b769bd66679a53932c4182e2c5b7496878f722e2
rhewjunior <fbckdals99@gmail.com>
2024-01-14 21:47:33 +0900

    [DOK-141] fix : 카테고리별 상품 조회 코드 수정

--- src/main/java/com/dokebi/dalkom/domain/product/entity/Product.java
+++ src/main/java/com/dokebi/dalkom/domain/product/entity/Product.java
@@ -1,59 +1,28 @@
 package com.dokebi.dalkom.domain.product.entity;
 
-<<<<<<< HEAD
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
-import com.dokebi.dalkom.common.EntityDate;
-
-=======
 import com.dokebi.dalkom.common.EntityDate;
 import com.dokebi.dalkom.domain.cart.entity.OrderCart;
 import com.dokebi.dalkom.domain.category.entity.Category;
 import com.dokebi.dalkom.domain.order.entity.OrdrDetail;
 import com.dokebi.dalkom.domain.stock.entity.PrdtStock;
->>>>>>> 73067a46a1ebad7347c62748751c5f01dce1b569
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-<<<<<<< HEAD
-@Setter
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Product extends EntityDate {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long productSeq;
-
-	@Column(nullable = false)
-	private Long categorySeq;
-
-	@Column(nullable = false)
-	private String company;
-
-	@Column(nullable = false)
-	private String imageUrl;
-
-	@Column(nullable = false)
-	private String info;
-
-	@Column(nullable = false)
-	private String name;
-
-	@Column(nullable = false)
-	private Integer price;
-
-	@Column(nullable = false)
-	private Character state;
-=======
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
@@ -62,43 +31,42 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Product extends EntityDate {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long productSeq;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long productSeq;
 
-    @ManyToOne
-    @JoinColumn(name = "categorySeq")
-    private Category category;
+	@ManyToOne
+	@JoinColumn(name = "categorySeq")
+	private Category category;
 
-    @Column(name = "name", nullable = false)
-    private String name;
+	@Column(name = "name", nullable = false)
+	private String name;
 
-    @Column(name = "price", nullable = false)
-    private Integer price;
+	@Column(name = "price", nullable = false)
+	private Integer price;
 
-    @Column(name = "info", nullable = false)
-    private String info;
+	@Column(name = "info", nullable = false)
+	private String info;
 
-    @Column(name = "imageUrl", nullable = false)
-    private String imageUrl;
+	@Column(name = "imageUrl", nullable = false)
+	private String imageUrl;
 
-    @Column(name = "company", nullable = false)
-    private String company;
+	@Column(name = "company", nullable = false)
+	private String company;
 
-    @Column(name = "state", nullable = false, columnDefinition = "VARCHAR(1) DEFAULT 'Y'")
-    private String state;
+	@Column(name = "state", nullable = false, columnDefinition = "VARCHAR(1) DEFAULT 'Y'")
+	private String state;
 
-    @OneToMany(mappedBy = "product")
-    private List<PrdtStock> prdtStock = new ArrayList<>();
+	@OneToMany(mappedBy = "product")
+	private List<PrdtStock> prdtStock = new ArrayList<>();
 
-    @OneToMany(mappedBy = "product")
-    private List<PrdtImage> prdtImage = new ArrayList<>();
+	@OneToMany(mappedBy = "product")
+	private List<PrdtImage> prdtImage = new ArrayList<>();
 
-    @OneToMany(mappedBy = "product")
-    private List<OrdrDetail> ordrDetail = new ArrayList<>();
+	@OneToMany(mappedBy = "product")
+	private List<OrdrDetail> ordrDetail = new ArrayList<>();
 
-    @OneToMany(mappedBy = "product")
-    private List<OrderCart> OrderCart = new ArrayList<>();
+	@OneToMany(mappedBy = "product")
+	private List<OrderCart> OrderCart = new ArrayList<>();
 
->>>>>>> 73067a46a1ebad7347c62748751c5f01dce1b569
 }
--- src/main/java/com/dokebi/dalkom/domain/product/repository/ProductRepository.java
+++ src/main/java/com/dokebi/dalkom/domain/product/repository/ProductRepository.java
@@ -2,15 +2,15 @@ package com.dokebi.dalkom.domain.product.repository;
 
 import java.util.List;
 
-import com.dokebi.dalkom.domain.product.dto.ProductByCategoryResponse;
-import com.dokebi.dalkom.domain.product.entity.Product;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.dokebi.dalkom.domain.product.dto.ProductByCategoryResponse;
+import com.dokebi.dalkom.domain.product.entity.Product;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	@Query("select p.productSeq, p.name, p.price, p.state, p.imageUrl, p.company, ps.amount AS stock FROM product p "
-		+ "LEFT JOIN prdtStock ps ON p.productSeq = ps.productSeq WHERE p.categorySeq = :categorySeq")
+		+ "LEFT JOIN prdtStock ps WHERE p.category.categorySeq = :categorySeq")
 	List<ProductByCategoryResponse> getProductsByCategory(@Param("categorySeq") Long categorySeq);
 }
