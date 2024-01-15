7808180aad4b94e9f85bc44911bdf33d3704107b
root <k3510h@hanmail.net>
2024-01-15 13:28:06 +0900

    [DOK-107] refactor : 약어 마저 제거

--- src/main/java/com/dokebi/dalkom/domain/product/entity/Product.java
+++ src/main/java/com/dokebi/dalkom/domain/product/entity/Product.java
@@ -1,18 +1,28 @@
 package com.dokebi.dalkom.domain.product.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 import com.dokebi.dalkom.common.EntityDate;
 import com.dokebi.dalkom.domain.cart.entity.OrderCart;
 import com.dokebi.dalkom.domain.category.entity.Category;
 import com.dokebi.dalkom.domain.order.entity.OrderDetail;
 import com.dokebi.dalkom.domain.stock.entity.ProductStock;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
@@ -47,14 +57,14 @@ public class Product extends EntityDate {
 	@Column(name = "state", nullable = false, columnDefinition = "VARCHAR(1) DEFAULT 'Y'")
 	private String state;
 
-    @OneToMany(mappedBy = "product")
-    private List<ProductStock> productStock = new ArrayList<>();
+	@OneToMany(mappedBy = "product")
+	private List<ProductStock> productStock = new ArrayList<>();
 
-    @OneToMany(mappedBy = "product")
-    private List<ProductImage> productImage = new ArrayList<>();
+	@OneToMany(mappedBy = "product")
+	private List<ProductImage> productImage = new ArrayList<>();
 
-    @OneToMany(mappedBy = "product")
-    private List<OrderDetail> ordrDetail = new ArrayList<>();
+	@OneToMany(mappedBy = "product")
+	private List<OrderDetail> orderDetailList = new ArrayList<>();
 
 	@OneToMany(mappedBy = "product")
 	private List<OrderCart> OrderCart = new ArrayList<>();
--- src/main/java/com/dokebi/dalkom/domain/stock/entity/ProductStockHistory.java
+++ src/main/java/com/dokebi/dalkom/domain/stock/entity/ProductStockHistory.java
@@ -1,12 +1,20 @@
 package com.dokebi.dalkom.domain.stock.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import com.dokebi.dalkom.common.EntityDate;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.persistence.*;
 
 @Entity
 @Getter
@@ -15,17 +23,17 @@ import javax.persistence.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProductStockHistory extends EntityDate {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long productStockHistorySeq;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long productStockHistorySeq;
 
-    @ManyToOne
-    @JoinColumn(name = "prdtStockSeq")
-    private ProductStock prdtStock;
+	@ManyToOne
+	@JoinColumn(name = "prdtStockSeq")
+	private ProductStock productStock;
 
-    @Column(name = "amount", nullable = false)
-    private Integer amount;
+	@Column(name = "amount", nullable = false)
+	private Integer amount;
 
-    @Column(name = "amountChanged", nullable = false)
-    private Integer amountChanged;
+	@Column(name = "amountChanged", nullable = false)
+	private Integer amountChanged;
 }
