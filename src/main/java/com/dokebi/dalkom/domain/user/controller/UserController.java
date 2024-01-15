a23322589c8b154218500b5b756c5586dc0df5bb
DJ._.Dazzling <44766266+allrightDJ0108@users.noreply.github.com>
2024-01-15 20:34:37 +0900

    [DOK-100] fix : 코드스멜

--- src/main/java/com/dokebi/dalkom/domain/user/controller/UserController.java
+++ src/main/java/com/dokebi/dalkom/domain/user/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.dokebi.dalkom.domain.user.controller;
 
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -11,8 +10,4 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class UserController {
 
-	@PutMapping("/api/user/")
-	public void updateUser() {
-
-	}
 }
