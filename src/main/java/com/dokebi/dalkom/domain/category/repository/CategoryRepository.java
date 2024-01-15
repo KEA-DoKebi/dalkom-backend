9fe78923065e1a0c98edb23120067e90eb5c4986
root <k3510h@hanmail.net>
2024-01-15 11:30:18 +0900

    [DOK-107] fix : 쿼리 수정

--- src/main/java/com/dokebi/dalkom/domain/category/repository/CategoryRepository.java
+++ src/main/java/com/dokebi/dalkom/domain/category/repository/CategoryRepository.java
@@ -11,9 +11,9 @@ import com.dokebi.dalkom.domain.category.dto.SubCategoryResponse;
 import com.dokebi.dalkom.domain.category.entity.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-	@Query("SELECT c.categorySeq, c.name, c.imageUrl FROM category c WHERE c.parentSeq IS NULL")
+	@Query("SELECT c.categorySeq, c.name, c.imageUrl FROM Category c WHERE c.parentSeq IS NULL")
 	List<CategoryResponse> getCategoryList();
 
-	@Query("SELECT c.categorySeq, c.name FROM category c WHERE c.parentSeq = :categorySeq")
+	@Query("SELECT c.categorySeq, c.name FROM Category c WHERE c.parentSeq = :categorySeq")
 	List<SubCategoryResponse> getSubCategoryList(@Param("categorySeq") Long categorySeq);
 }
