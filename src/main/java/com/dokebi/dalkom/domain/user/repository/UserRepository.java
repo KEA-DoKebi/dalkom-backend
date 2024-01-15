4e53712aaeac54e93d22f829f40ed087feabcd5c
DJ._.Dazzling <44766266+allrightDJ0108@users.noreply.github.com>
2024-01-15 20:24:19 +0900

    [DOK-100] fix : 오류 수정

--- src/main/java/com/dokebi/dalkom/domain/user/repository/UserRepository.java
+++ src/main/java/com/dokebi/dalkom/domain/user/repository/UserRepository.java
@@ -6,12 +6,15 @@ import com.dokebi.dalkom.domain.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	User findByUserSeq(Long userSeq);
+
 	User findByEmail(String email);
 
 	boolean existsByEmail(String email);
 
 	boolean existsByNickname(String nickname);
 	// boolean existsByNickname(String nickname);
+
 	//
 	// @Modifying
 	// @Query("UPDATE User u SET u.password = :password, u.nickname = :nickname, u.profileImgSeq = :profileImgSeq WHERE u.userSeq = :userSeq")
