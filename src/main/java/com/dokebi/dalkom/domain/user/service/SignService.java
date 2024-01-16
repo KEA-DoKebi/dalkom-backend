74f3ab69b3000af354801ce30ddcfd646be24cfb
suha <hp980724@gmail.com>
2024-01-16 10:07:54 +0900

    [DOK-70] feat : 주문 조회 기능 추가

--- src/main/java/com/dokebi/dalkom/domain/user/service/SignService.java
+++ src/main/java/com/dokebi/dalkom/domain/user/service/SignService.java
@@ -1,7 +1,5 @@
 package com.dokebi.dalkom.domain.user.service;
 
-//import org.springframework.security.crypto.password.PasswordEncoder;
- 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,13 +16,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SignService {
 
-    @Transactional
-    //생성일 삭제일 추가
-    public void signUp(SignUpRequest req) {
-        // validateSignUpInfo(req);
-        // User user = userRepository.save(SignUpRequest.toEntity(req, passwordEncoder));
-    }
- 
 	private final UserRepository userRepository;
 	private final EmployeeRepository employeeRepository;
 	//private final PasswordEncoder passwordEncoder;
@@ -114,6 +105,5 @@ public class SignService {
 	// 		throw new LoginFailureException();
 	// 	}
 	// }
- 
 
 }
