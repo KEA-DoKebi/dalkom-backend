91349d8fa4e338365dac0d7bd1a75a3ba0d56a2e
root <k3510h@hanmail.net>
2024-01-16 17:36:09 +0900

    [DOK-110] feat : 옵션 코드/옵션 상세 목록 조회

---
+++ src/main/java/com/dokebi/dalkom/domain/option/dto/OptionCodeResponse.java
@@ -0,0 +1,14 @@
+package com.dokebi.dalkom.domain.option.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OptionCodeResponse {
+	private String optionCode;
+}
--- src/main/java/com/dokebi/dalkom/domain/option/repository/ProductOptionRepository.java
+++ src/main/java/com/dokebi/dalkom/domain/option/repository/ProductOptionRepository.java
@@ -1,8 +1,19 @@
 package com.dokebi.dalkom.domain.option.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.dokebi.dalkom.domain.option.entity.ProductOption;
 
+import io.lettuce.core.dynamic.annotation.Param;
+
 public interface ProductOptionRepository extends JpaRepository<ProductOption, Long> {
+
+	@Query("SELECT po.optionCode FROM ProductOption po GROUP BY po.optionCode")
+	List<String> findAllOptionCode();
+
+	@Query("SELECT po.detail FROM ProductOption po WHERE po.optionCode =:optionCode")
+	List<String> findDetailByOptionCode(@Param("optionCode") String optionCode);
 }
