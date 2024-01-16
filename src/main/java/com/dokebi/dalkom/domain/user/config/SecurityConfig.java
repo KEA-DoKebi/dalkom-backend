515957a35e6de151f8832dc6e64d13b1d7e2138a
DJ._.Dazzling <44766266+allrightDJ0108@users.noreply.github.com>
2024-01-16 13:48:23 +0900

    [DOK-158] feat : securityconfig

---
+++ src/main/java/com/dokebi/dalkom/domain/user/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.dokebi.dalkom.domain.user.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
