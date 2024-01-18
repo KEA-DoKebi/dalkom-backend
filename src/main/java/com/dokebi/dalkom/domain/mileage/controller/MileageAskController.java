c76a11bf150add68e7ae80c33a6007001b385b6c
Rhew Changmeen <73987361+changmeen@users.noreply.github.com>
2024-01-18 14:39:35 +0900

    import org.springframework.http.HttpStatus; 추가

--- src/main/java/com/dokebi/dalkom/domain/mileage/controller/MileageAskController.java
+++ src/main/java/com/dokebi/dalkom/domain/mileage/controller/MileageAskController.java
@@ -1,5 +1,6 @@
 package com.dokebi.dalkom.domain.mileage.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
