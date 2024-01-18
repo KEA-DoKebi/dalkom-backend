1ede7f7c94d5d53e9d2103be5ee91499211566ee
DJ._.Dazzling <44766266+allrightDJ0108@users.noreply.github.com>
2024-01-18 19:09:54 +0900

    [back-branch-dok-158] fix : 충돌 수정

--- src/main/java/com/dokebi/dalkom/domain/admin/repository/AdminRepository.java
+++ src/main/java/com/dokebi/dalkom/domain/admin/repository/AdminRepository.java
@@ -9,9 +9,7 @@ import com.dokebi.dalkom.domain.admin.entity.Admin;
 public interface AdminRepository extends JpaRepository<Admin, Long> {
 	boolean existsByNickname(String nickname);
 
-	Admin findByAdminSeq(Long adminSeq);
-
 	Admin findByAdminId(String adminId);
-	
+
 	Optional<Admin> findByAdminSeq(Long adminSeq);
 }
