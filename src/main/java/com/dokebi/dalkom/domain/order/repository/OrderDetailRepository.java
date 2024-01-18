3faa732e46fbcd411d5b48c8fc68e2a7766e11e5
suha <hp980724@gmail.com>
2024-01-18 16:12:58 +0900

    [DOK-70] Update OrderDetailRepository.java

--- src/main/java/com/dokebi/dalkom/domain/order/repository/OrderDetailRepository.java
+++ src/main/java/com/dokebi/dalkom/domain/order/repository/OrderDetailRepository.java
@@ -1,8 +1,6 @@
 package com.dokebi.dalkom.domain.order.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
-
 import com.dokebi.dalkom.domain.order.entity.OrderDetail;
 
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
