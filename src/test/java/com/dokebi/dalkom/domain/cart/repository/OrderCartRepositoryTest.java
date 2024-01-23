2e9db4c677cf571751d08eb921861ffdf3524802
DJ._.Dazzling <44766266+allrightDJ0108@users.noreply.github.com>
2024-01-23 14:07:46 +0900

    [DOK-207] fix : 장바구니 페이지네이션 추가

--- src/test/java/com/dokebi/dalkom/domain/cart/repository/OrderCartRepositoryTest.java
+++ src/test/java/com/dokebi/dalkom/domain/cart/repository/OrderCartRepositoryTest.java
@@ -10,6 +10,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import com.dokebi.dalkom.domain.cart.dto.OrderCartReadResponse;
 import com.dokebi.dalkom.domain.cart.factory.OrderCartReadResponseFactory;
@@ -23,19 +27,23 @@ public class OrderCartRepositoryTest {
 	void findOrderCartListTest() {
 		// Given
 		Long userSeq = 1L;
+		Pageable pageable = PageRequest.of(0, 3); // 첫 번째 페이지, 페이지 당 3개 항목
 
 		OrderCartReadResponse fakeResponse1 = OrderCartReadResponseFactory.createOrderCartReadResponse();
 		OrderCartReadResponse fakeResponse2 = OrderCartReadResponseFactory.createOrderCartReadResponse();
 		OrderCartReadResponse fakeResponse3 = OrderCartReadResponseFactory.createOrderCartReadResponse();
 
 		List<OrderCartReadResponse> responseList = Arrays.asList(fakeResponse1, fakeResponse2, fakeResponse3);
+		Page<OrderCartReadResponse> responsePage = new PageImpl<>(responseList, pageable, responseList.size());
 
-		when(orderCartRepository.findOrderCartList(userSeq)).thenReturn(responseList);
+		when(orderCartRepository.findOrderCartList(userSeq, pageable)).thenReturn(responsePage);
 
 		// When
-		List<OrderCartReadResponse> orderCartReadResponseList = orderCartRepository.findOrderCartList(userSeq);
+		Page<OrderCartReadResponse> orderCartReadResponsePage = orderCartRepository.findOrderCartList(userSeq,
+			pageable);
 
 		// Then
-		assertEquals(responseList, orderCartReadResponseList);
+		assertEquals(responsePage, orderCartReadResponsePage);
+
 	}
 }
