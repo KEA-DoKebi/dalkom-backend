66c453dbce1ddf7f277d24c098836772a973e4ff
DJ._.Dazzling <44766266+allrightDJ0108@users.noreply.github.com>
2024-01-23 14:20:24 +0900

    [DOK-207] fix : 공지사항 목록 페이지네이션 추가

--- src/main/java/com/dokebi/dalkom/domain/notice/repository/NoticeRepository.java
+++ src/main/java/com/dokebi/dalkom/domain/notice/repository/NoticeRepository.java
@@ -1,7 +1,7 @@
 package com.dokebi.dalkom.domain.notice.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -23,5 +23,5 @@ public interface NoticeRepository extends JpaRepository<Notice, Long> {
 	@Query("SELECT NEW com.dokebi.dalkom.domain.notice.dto.NoticeListResponse("
 		+ "n.noticeSeq, n.title, n.content, n.createdAt, n.modifiedAt, n.admin.nickname, n.state) " +
 		"FROM Notice n ")
-	List<NoticeListResponse> findNoticeList();
+	Page<NoticeListResponse> findNoticeList(Pageable pageable);
 }
