a9fabfe654c2976759b687d4f41d243a35cd3f20
root <xornjswldh@naver.com>
2024-01-23 16:53:41 +0900

    [DOK-177] test : DTO 테스트

--- src/main/java/com/dokebi/dalkom/domain/order/dto/OrderStateUpdateRequest.java
+++ src/main/java/com/dokebi/dalkom/domain/order/dto/OrderStateUpdateRequest.java
@@ -3,8 +3,10 @@ package com.dokebi.dalkom.domain.order.dto;
 import javax.validation.constraints.Pattern;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class OrderStateUpdateRequest {
 
 	@Pattern(regexp = "^(1[0-9]|[2-9][0-9]|100)$", message = "OrderStateUpdateRequest orderState pattern 에러")
