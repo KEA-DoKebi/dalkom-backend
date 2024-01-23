14692fc0d01e514bf199cf677b76d461c96e2855
Rhew Changmeen <73987361+changmeen@users.noreply.github.com>
2024-01-23 22:52:13 +0900

    불필요한 공백 제거

--- src/test/java/com/dokebi/dalkom/domain/inquiry/factory/OrderStateUpdateRequestFactory.java
+++ src/test/java/com/dokebi/dalkom/domain/inquiry/factory/OrderStateUpdateRequestFactory.java
@@ -4,7 +4,6 @@ import com.dokebi.dalkom.domain.inquiry.dto.InquiryAnswerRequest;
 import com.dokebi.dalkom.domain.order.dto.OrderStateUpdateRequest;
 
 public class OrderStateUpdateRequestFactory {
-
 	public static OrderStateUpdateRequest createOrderStateUpdateRequest(String orderState) {
 		return new OrderStateUpdateRequest(orderState);
 	}
