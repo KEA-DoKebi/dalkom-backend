36ba7c456a222e20bd60aa6e98b01e5003750b6e
root <xornjswldh@naver.com>
2024-01-24 15:59:08 +0900

    [DOK-216] fix : 상품 관련 API 수정에 따른 DTO 수정

--- src/main/java/com/dokebi/dalkom/domain/product/dto/ProductByCategoryDetailResponse.java
+++ src/main/java/com/dokebi/dalkom/domain/product/dto/ProductByCategoryDetailResponse.java
@@ -1,7 +1,5 @@
 package com.dokebi.dalkom.domain.product.dto;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,5 +17,6 @@ public class ProductByCategoryDetailResponse {
 	private String state;
 	private String imageUrl;
 	private String company;
-	private Integer amount;
+	private Double rating; // 리뷰 테이블에서 입력 받은 productSeq와 같은 데이터의 평점의 평균
+	private Long reviewAmount; // 리뷰 테이블에서 입력받은 productSeq와 같은 데이터의 개수
 }
