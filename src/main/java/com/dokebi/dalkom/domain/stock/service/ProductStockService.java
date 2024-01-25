2a5fd3d8c3be8a939bcf0bebb2f316e09122f59d
root <xornjswldh@naver.com>
2024-01-25 15:54:07 +0900

    [DOK-107] fix : 순환 참조 해결

--- src/main/java/com/dokebi/dalkom/domain/stock/service/ProductStockService.java
+++ src/main/java/com/dokebi/dalkom/domain/stock/service/ProductStockService.java
@@ -2,10 +2,12 @@ package com.dokebi.dalkom.domain.stock.service;
 
 import java.util.List;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dokebi.dalkom.domain.product.service.ProductService;
+import com.dokebi.dalkom.domain.stock.dto.StockListDto;
 import com.dokebi.dalkom.domain.stock.entity.ProductStock;
 import com.dokebi.dalkom.domain.stock.entity.ProductStockHistory;
 import com.dokebi.dalkom.domain.stock.exception.InvalidAmountException;
@@ -14,15 +16,22 @@ import com.dokebi.dalkom.domain.stock.exception.ProductStockNotFoundException;
 import com.dokebi.dalkom.domain.stock.repository.ProductStockHistoryRepository;
 import com.dokebi.dalkom.domain.stock.repository.ProductStockRepository;
 
-import lombok.RequiredArgsConstructor;
-
 @Service
 @Transactional
-@RequiredArgsConstructor
+// @RequiredArgsConstructor
 public class ProductStockService {
 	private final ProductStockRepository stockRepository;
 	private final ProductStockHistoryRepository stockHistoryRepository;
 	private final ProductService productService;
+
+	// 순환 참조 해결을 위해 생성자를 통해 @Lazy 사용
+	public ProductStockService(ProductStockRepository stockRepository,
+		ProductStockHistoryRepository stockHistoryRepository,
+		@Lazy ProductService productService) {
+		this.stockRepository = stockRepository;
+		this.stockHistoryRepository = stockHistoryRepository;
+		this.productService = productService;
+	}
 
 	public void updateStock(Long stockSeq, Integer amount) {
 		ProductStock stock = stockRepository.findById(stockSeq).orElseThrow(ProductStockNotFoundException::new);
@@ -72,6 +81,10 @@ public class ProductStockService {
 		if (stock.getAmount() - amountChanged < 0) {
 			throw new NotEnoughStockException();
 		}
+	}
+
+	public List<StockListDto> readStockListDtoByProductSeq(Long productSeq) {
+		return stockRepository.findStockListDtoByProductSeq(productSeq);
 	}
 
 	@Transactional(readOnly = true)
