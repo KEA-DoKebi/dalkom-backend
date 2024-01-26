cde60d4d9b4e83e1c0ae34c6c212062b8767393b
suha <hp980724@gmail.com>
2024-01-26 16:38:59 +0900

    [DOK-229] Update OrderUserReadResponseTest.java

--- src/test/java/com/dokebi/dalkom/domain/order/dto/OrderUserReadResponseTest.java
+++ src/test/java/com/dokebi/dalkom/domain/order/dto/OrderUserReadResponseTest.java
@@ -10,10 +10,6 @@ class OrderUserReadResponseTest {
 	void createOrderDto() {
 		// Given
 		Long expectedOrdrSeq = 1L;
-		String expectedReceiverName = "홍길동";
-		String expectedReceiverAddress = "서울시 강남구";
-		String expectedReceiverMobileNum = "010-1234-5678";
-		String expectedReceiverMemo = "부재 시 문 앞에 놓아주세요";
 		Integer expectedTotalPrice = 10000;
 
 		// When
@@ -21,10 +17,6 @@ class OrderUserReadResponseTest {
 
 		// Then
 		assertThat(orderUserReadResponse.getOrdrSeq()).isEqualTo(expectedOrdrSeq);
-		assertThat(orderUserReadResponse.getReceiverName()).isEqualTo(expectedReceiverName);
-		assertThat(orderUserReadResponse.getReceiverAddress()).isEqualTo(expectedReceiverAddress);
-		assertThat(orderUserReadResponse.getReceiverMobileNum()).isEqualTo(expectedReceiverMobileNum);
-		assertThat(orderUserReadResponse.getReceiverMemo()).isEqualTo(expectedReceiverMemo);
 		assertThat(orderUserReadResponse.getTotalPrice()).isEqualTo(expectedTotalPrice);
 	}
 }
