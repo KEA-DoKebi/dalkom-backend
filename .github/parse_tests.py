a7e77dc6a4454340481a9ef627ab3d4c729ece17
Kim Donghyeon <eastlighting1@gachon.ac.kr>
2024-01-26 19:50:34 +0900

    workflow : 경로 수정

--- .github/parse_tests.py
+++ .github/parse_tests.py
@@ -1,23 +1,25 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
+import glob
 
-def parse_junit_xml(file_path):
-    tree = ET.parse(file_path)
-    root = tree.getroot()
-
+def parse_junit_xmls(file_paths):
     test_cases = []
 
-    for testcase in root.iter('testcase'):
-        class_name = testcase.attrib['classname']
-        method_name = testcase.attrib['name']
-        time = testcase.attrib['time']
-        status = 'PASSED'
+    for file_path in file_paths:
+        tree = ET.parse(file_path)
+        root = tree.getroot()
 
-        for child in testcase:
-            if child.tag == 'failure':
-                status = 'FAILED'
+        for testcase in root.iter('testcase'):
+            class_name = testcase.attrib['classname']
+            method_name = testcase.attrib['name']
+            time = testcase.attrib['time']
+            status = 'PASSED'
 
-        test_cases.append([class_name, method_name, status, time])
+            for child in testcase:
+                if child.tag == 'failure':
+                    status = 'FAILED'
+
+            test_cases.append([class_name, method_name, status, time])
 
     return test_cases
 
@@ -28,11 +30,11 @@ def generate_markdown_table(test_cases, output_file):
     with open(output_file, 'w') as file:
         file.write(markdown_table)
 
-# JUnit 결과 XML 파일 경로. 이 경로는 실제 경로로 대체해야 합니다.
-junit_xml_file_path = 'path/to/your/junit-results.xml'
+# JUnit 결과 XML 파일들을 찾는 경로
+junit_xml_files_path = './build/test-results/test/TEST-*.xml'
 
 # 테스트 케이스 파싱
-test_cases = parse_junit_xml(junit_xml_file_path)
+test_cases = parse_junit_xmls(glob.glob(junit_xml_files_path))
 
 # Markdown 파일 생성
 generate_markdown_table(test_cases, 'test_list.md')
