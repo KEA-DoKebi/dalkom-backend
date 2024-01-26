ce0124f81069443d2e9db0dbd37fc0168ab26951
root <xornjswldh@naver.com>
2024-01-26 14:56:22 +0900

    [DOK-107] feat : 상품 상태를 3개(판매중, 품절, 판매끝)으로 확장

--- src/main/java/com/dokebi/dalkom/common/magicNumber/ProductActiveState.java
+++
@@ -1,6 +0,0 @@
-package com.dokebi.dalkom.common.magicNumber;
-
-public class ProductActiveState {
-	public static final String ACTIVE = "Y";
-	public static final String INACTIVE = "N";
-}
---
+++ src/main/java/com/dokebi/dalkom/common/magicnumber/ProductActiveState.java
@@ -0,0 +1,13 @@
+package com.dokebi.dalkom.common.magicnumber;
+
+public class ProductActiveState {
+
+	// 판매중, 사용자에게 표시됨
+	public static final String ACTIVE = "Y";
+
+	// 판매 안함, 사용자에게 표시됨
+	public static final String SOLDOUT = "S";
+
+	// 판매 안함, 사용자에게 표시 안됨
+	public static final String INACTIVE = "N";
+}
