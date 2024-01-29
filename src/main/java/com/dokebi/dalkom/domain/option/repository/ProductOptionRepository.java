a28959bd667e108ce54ccf34b628da87672505a3
root <xornjswldh@naver.com>
2024-01-29 11:24:05 +0900

    [DOK-107] fix : option과 stock 매칭

--- src/main/java/com/dokebi/dalkom/domain/option/repository/ProductOptionRepository.java
+++ src/main/java/com/dokebi/dalkom/domain/option/repository/ProductOptionRepository.java
@@ -5,12 +5,10 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.dokebi.dalkom.domain.option.dto.OptionCodeResponse;
-import com.dokebi.dalkom.domain.option.dto.OptionListDto;
 import com.dokebi.dalkom.domain.option.entity.ProductOption;
-
-import io.lettuce.core.dynamic.annotation.Param;
 
 public interface ProductOptionRepository extends JpaRepository<ProductOption, Long> {
 
@@ -20,14 +18,6 @@ public interface ProductOptionRepository extends JpaRepository<ProductOption, Lo
 
 	@Query("SELECT po.detail FROM ProductOption po WHERE po.optionCode =:optionCode")
 	List<String> findDetailByOptionCode(@Param("optionCode") String optionCode);
-
-	@Query("SELECT NEW com.dokebi.dalkom.domain.option.dto.OptionListDto( "
-		+ "po.prdtOptionSeq, po.detail) "
-		+ "FROM ProductOption po "
-		+ "INNER JOIN ProductStock ps "
-		+ "ON po.prdtOptionSeq = ps.productOption.prdtOptionSeq "
-		+ "AND ps.product.productSeq = :productSeq ")
-	List<OptionListDto> findOptionListDtoByProductSeq(@Param("productSeq") Long productSeq);
 
 	Optional<ProductOption> findProductOptionByPrdtOptionSeq(Long prdtOptionSeq);
 }
