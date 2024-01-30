0a8913d8bf77e05622812090ed356005e19b00be
root <xornjswldh@naver.com>
2024-01-30 10:03:18 +0900

    [DOK-282] fix : 상수에 final 추가

--- src/main/java/com/dokebi/dalkom/common/magicnumber/OrderState.java
+++ src/main/java/com/dokebi/dalkom/common/magicnumber/OrderState.java
@@ -1,15 +1,15 @@
 package com.dokebi.dalkom.common.magicnumber;
 
 public class OrderState {
-	public static String CONFIRMED = "11"; // 주문확인
-	public static String PREPARING = "12"; // 배송준비
-	public static String SHIPPED = "13"; // 배송시작
-	public static String DELIVERED = "14"; // 배송완료
-	public static String FINALIZED = "15"; // 구매확정
-	public static String CANCELED = "21"; // 주문취소
-	public static String REFUND_CONFIRMED = "31"; // 환불접수
-	public static String RETURNED = "32"; // 반송완료
-	public static String REFUNDED = "33"; // 환불완료
+	public static final String CONFIRMED = "11"; // 주문확인
+	public static final String PREPARING = "12"; // 배송준비
+	public static final String SHIPPED = "13"; // 배송시작
+	public static final String DELIVERED = "14"; // 배송완료
+	public static final String FINALIZED = "15"; // 구매확정
+	public static final String CANCELED = "21"; // 주문취소
+	public static final String REFUND_CONFIRMED = "31"; // 환불접수
+	public static final String RETURNED = "32"; // 반송완료
+	public static final String REFUNDED = "33"; // 환불완료
 
 	//"11" : 주문확인, "12" : 배송준비, "13" : 배송시작, "14" : 배송완료, "15" : 구매확정,
 	// "21" : 주문취소,
