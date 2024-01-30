46c30d2de7af78d63e2605bf7a3d4119fd65dacd
suha <hp980724@gmail.com>
2024-01-30 15:37:06 +0900

    [DOK-70] feat : 주문시 장바구니 상품 제거 완료

--- src/main/java/com/dokebi/dalkom/domain/order/service/OrderService.java
+++ src/main/java/com/dokebi/dalkom/domain/order/service/OrderService.java
@@ -13,6 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.dokebi.dalkom.common.magicnumber.MileageHistoryState;
 import com.dokebi.dalkom.common.magicnumber.OrderState;
+import com.dokebi.dalkom.domain.cart.dto.OrderCartDeleteRequest;
+import com.dokebi.dalkom.domain.cart.service.OrderCartService;
 import com.dokebi.dalkom.domain.mileage.exception.MileageLackException;
 import com.dokebi.dalkom.domain.mileage.service.MileageService;
 import com.dokebi.dalkom.domain.option.entity.ProductOption;
@@ -58,16 +60,24 @@ public class OrderService {
 	private final UserService userService;
 	private final ReviewService reviewService;
 	private final PasswordEncoder passwordEncoder;
+	private final OrderCartService orderCartService;
 
 	// 결제 하기
 	@Transactional
 	public Integer createOrder(Long userSeq, OrderCreateRequest request) {
 
 		int orderTotalPrice = 0;
-
+		OrderCartDeleteRequest orderCartDeleteRequest = new OrderCartDeleteRequest(new ArrayList<>());
+		System.out.println("---------------------------------------------------------------");
+		System.out.println(request);
+		System.out.println("---------------------------------------------------------------");
 		// orderTotalPrice를 먼저 계산해준다.
 		for (OrderProductRequest orderProduct : request.getOrderProductRequestList()) {
 			orderTotalPrice += calculateProductPrice(orderProduct);
+		}
+		for (OrderProductRequest orderProduct : request.getOrderProductRequestList()) {
+			Long orderCartSeq = orderProduct.getOrderCartSeq();
+			orderCartDeleteRequest.getOrderCartSeqList().add(orderCartSeq);
 		}
 
 		// 사용자 정보 조회
@@ -99,6 +109,9 @@ public class OrderService {
 			Integer totalMileage = (user.getMileage() - orderTotalPrice);
 
 			mileageService.createMileageHistory(user, orderTotalPrice, totalMileage, MileageHistoryState.USED);
+
+			//장바구니의 상품 삭제
+			orderCartService.deleteOrderCart(orderCartDeleteRequest);
 
 			// 사용자의 마일리지 업데이트
 			user.setMileage(totalMileage);
