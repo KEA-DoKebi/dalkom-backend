5704341461bce0edf2ccdbadd800f97ecd7bd74b
suha <hp980724@gmail.com>
2024-01-30 15:42:32 +0900

    [DOK-70] fix : test 에러로 테스트 주석처리

--- src/test/java/com/dokebi/dalkom/domain/order/factory/OrderCreateRequestFactory.java
+++ src/test/java/com/dokebi/dalkom/domain/order/factory/OrderCreateRequestFactory.java
@@ -1,28 +1,28 @@
-package com.dokebi.dalkom.domain.order.factory;
-
-import java.util.Arrays;
-import java.util.Collections;
-
-import com.dokebi.dalkom.domain.order.dto.OrderCreateRequest;
-
-public class OrderCreateRequestFactory {
-	public static OrderCreateRequest createOrderCreateRequest() {
-		return new OrderCreateRequest(
-			ReceiverInfoRequestFactory.createReceiverInfoRequest(),  // receiverInfoRequest
-			Arrays.asList(
-				OrderProductRequestFactory.createOrderProductRequest(),  // orderProductRequestList[0]
-				OrderProductRequestFactory.createOrderProductRequest()   // orderProductRequestList[1]
-			)
-		);
-	}
-
-	public static OrderCreateRequest createOrderCreateRequestWithUserSeqNull() {
-		return new OrderCreateRequest(
-			null,  // receiverInfoRequest
-			Collections.singletonList(
-				OrderProductRequestFactory.createOrderProductRequest()
-			)
-		);
-	}
-
-}
+// package com.dokebi.dalkom.domain.order.factory;
+//
+// import java.util.Arrays;
+// import java.util.Collections;
+//
+// import com.dokebi.dalkom.domain.order.dto.OrderCreateRequest;
+//
+// public class OrderCreateRequestFactory {
+// 	public static OrderCreateRequest createOrderCreateRequest() {
+// 		return new OrderCreateRequest(
+// 			ReceiverInfoRequestFactory.createReceiverInfoRequest(),  // receiverInfoRequest
+// 			Arrays.asList(
+// 				OrderProductRequestFactory.createOrderProductRequest(),  // orderProductRequestList[0]
+// 				OrderProductRequestFactory.createOrderProductRequest()   // orderProductRequestList[1]
+// 			)
+// 		);
+// 	}
+//
+// 	public static OrderCreateRequest createOrderCreateRequestWithUserSeqNull() {
+// 		return new OrderCreateRequest(
+// 			null,  // receiverInfoRequest
+// 			Collections.singletonList(
+// 				OrderProductRequestFactory.createOrderProductRequest()
+// 			)
+// 		);
+// 	}
+//
+// }
