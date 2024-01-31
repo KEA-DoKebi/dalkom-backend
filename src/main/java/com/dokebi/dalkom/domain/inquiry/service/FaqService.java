d3c965dcdb992f8ea46b1e6d72336c23db2574a5
root <xornjswldh@naver.com>
2024-01-31 09:45:42 +0900

    [DOK-241] fix : delete 메서드 try-catch 구조로 일괄변경

--- src/main/java/com/dokebi/dalkom/domain/inquiry/service/FaqService.java
+++ src/main/java/com/dokebi/dalkom/domain/inquiry/service/FaqService.java
@@ -2,6 +2,7 @@ package com.dokebi.dalkom.domain.inquiry.service;
 
 import java.util.Optional;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -65,6 +66,10 @@ public class FaqService {
 
 	@Transactional
 	public void deleteFaq(Long inquirySeq) {
-		faqRepository.deleteById(inquirySeq);
+		try {
+			faqRepository.deleteById(inquirySeq);
+		} catch (EmptyResultDataAccessException e) {
+			throw new FaqNotFoundException();
+		}
 	}
 }
