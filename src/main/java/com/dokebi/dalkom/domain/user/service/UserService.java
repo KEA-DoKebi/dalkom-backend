c1f98115617929c3e982e69d41473ff056afd344
root <xornjswldh@naver.com>
2024-01-31 17:09:32 +0900

    [DOK-241] fix : 사용되지 않는 부분 정리

--- src/main/java/com/dokebi/dalkom/domain/user/service/UserService.java
+++ src/main/java/com/dokebi/dalkom/domain/user/service/UserService.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,8 +24,6 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
 	private final UserRepository userRepository;
-
-	private final PasswordEncoder passwordEncoder;
 
 	@Transactional
 	public void updateUser(Long userSeq, UserUpdateRequest request) {
