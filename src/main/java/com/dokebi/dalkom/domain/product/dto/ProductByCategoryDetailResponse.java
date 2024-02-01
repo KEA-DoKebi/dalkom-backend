3125b5d2210f1a918d5b0fefb02f9f24be0c15bf
root <xornjswldh@naver.com>
2024-02-01 13:56:04 +0900

    [DOK-241] feat : 하위카테고리별 상품 리스트 반환시 카테고리 이름도 같이 반환

--- src/main/java/com/dokebi/dalkom/domain/product/dto/ProductByCategoryDetailResponse.java
+++ src/main/java/com/dokebi/dalkom/domain/product/dto/ProductByCategoryDetailResponse.java
@@ -1,6 +1,6 @@
 package com.dokebi.dalkom.domain.product.dto;
 
-import com.dokebi.dalkom.common.magicnumber.ProductActiveState;
+import org.springframework.data.domain.Page;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,26 +12,6 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductByCategoryDetailResponse {
-	private Long productSeq;
-	private String name;
-	private Integer price;
-	private String state;
-	private String stateName;
-	private String imageUrl;
-	private String company;
-	private Double rating; // 리뷰 테이블에서 입력 받은 productSeq와 같은 데이터의 평점의 평균
-	private Long reviewAmount; // 리뷰 테이블에서 입력받은 productSeq와 같은 데이터의 개수
-
-	public ProductByCategoryDetailResponse(Long productSeq, String name, Integer price, String state,
-		String imageUrl, String company, Double rating, Long reviewAmount) {
-		this.productSeq = productSeq;
-		this.name = name;
-		this.price = price;
-		this.state = state;
-		this.stateName = ProductActiveState.getNameByState((state));
-		this.imageUrl = imageUrl;
-		this.company = company;
-		this.rating = rating;
-		this.reviewAmount = reviewAmount;
-	}
+	private String categoryName;
+	private Page<ProductByCategoryDetailPage> page;
 }
