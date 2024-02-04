5ce7a85493813859d556eac74d9601f3037a68d7
rhewjunior <fbckdals99@gmail.com>
2024-02-04 22:17:25 +0900

    [DOK-87] feat : 문의 삭제 API 추가

--- src/main/java/com/dokebi/dalkom/domain/inquiry/controller/InquiryController.java
+++ src/main/java/com/dokebi/dalkom/domain/inquiry/controller/InquiryController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -74,4 +75,11 @@ public class InquiryController {
 		return Response.success(inquiryService.readInquiryListByCategorySearch(categorySeq, title, pageable));
 	}
 
+	// INQUIRY-008 (문의 삭제)
+	@DeleteMapping("/api/inquiry/{inquirySeq}")
+	@ResponseStatus(HttpStatus.OK)
+	public Response deleteInquiry(@PathVariable Long inquirySeq) {
+		inquiryService.deleteInquiry(inquirySeq);
+		return Response.success();
+	}
 }
