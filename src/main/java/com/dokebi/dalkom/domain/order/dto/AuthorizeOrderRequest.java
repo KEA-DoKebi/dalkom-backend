26a6f14cabe3b9c0b895998b264c40bbe90dd98f
suha <hp980724@gmail.com>
2024-02-05 18:28:00 +0900

    [DOK-85] feat : test 코드 추가

--- src/main/java/com/dokebi/dalkom/domain/order/dto/AuthorizeOrderRequest.java
+++ src/main/java/com/dokebi/dalkom/domain/order/dto/AuthorizeOrderRequest.java
@@ -3,12 +3,14 @@ package com.dokebi.dalkom.domain.order.dto;
 import javax.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class AuthorizeOrderRequest {
 	@NotBlank(message = "AuthorizeOrderRequest password NotBlank 에러")
 	private String password;
