f472e428d46c1b04876c09f2154cba6498743731
“mj” <75360915+minjun0707@users.noreply.github.com>
2024-02-05 10:39:25 +0900

    Squashed commit of the following:
    commit 75ceefe4a46ee7585525b070b5706646a4b5024e
    Merge: 7a85047 a4ece68
    Author: xornjswldh <86345585+xornjswldh@users.noreply.github.com>
    Date:   Mon Feb 5 10:38:55 2024 +0900
    
        Merge pull request #243 from KEA-DoKebi/DOK-96/cud-admin
    
        [DOK-96] 관리자가 직접 사용자를 추가할 때에도 마일리지 계산해서 자동으로 추가하도록 수정
    
    commit a4ece6829f1ad0ac8f101221a757cd380712eec2
    Merge: 33a0bb9 223d1b2
    Author: xornjswldh <86345585+xornjswldh@users.noreply.github.com>
    Date:   Mon Feb 5 10:32:36 2024 +0900
    
        Merge pull request #244 from KEA-DoKebi/DOK-89/review-CUD
    
        [DOK-89] 한명이 하나의 주문상세에 두개의 리뷰를 작성할 수 없도록 수정
    
    commit 223d1b2b86c1e3a5c3e58a7314aee3302b362081
    Author: root <xornjswldh@naver.com>
    Date:   Mon Feb 5 10:29:33 2024 +0900
    
        [DOK-89] feat : 한명이 하나의 주문상세에 두개의 리뷰를 작성할 수 없도록 수정
    
    commit 33a0bb9f9a09f3a509cb7b3776444a55241e5b69
    Author: root <xornjswldh@naver.com>
    Date:   Mon Feb 5 10:01:12 2024 +0900
    
        [DOK-96] fix : @NotBlank는 @NotNull을 포함함
    
    commit d8a1e4675a46724032436cdd9f58f9f3fa5e47fa
    Author: root <xornjswldh@naver.com>
    Date:   Mon Feb 5 09:52:25 2024 +0900
    
        [DOK-96] fix : 순환 빈 참조 해결
    
    commit 6452e137b8960268b70d363ca97a93f3264583d8
    Author: root <xornjswldh@naver.com>
    Date:   Mon Feb 5 09:48:06 2024 +0900
    
        [DOK-96] feat : 관리자가 사용자를 추가할 때 마일리지 계산해서 넣는 기능 추가

--- src/main/java/com/dokebi/dalkom/domain/user/dto/SignUpRequest.java
+++ src/main/java/com/dokebi/dalkom/domain/user/dto/SignUpRequest.java
@@ -20,34 +20,29 @@ import lombok.Setter;
 @NoArgsConstructor
 public class SignUpRequest {
 
-	@NotNull(message = "SignUpRequest empId notnull 에러")
 	@NotBlank(message = "SignUpRequest empId notblank 에러")
 	private String empId;
+
+	@NotBlank(message = "SignUpRequest email notblank 에러")
+	private String email;
 
 	@NotNull(message = "SignUpRequest password notnull 에러")
 	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$", message = "비밀번호는 최소 8자리이면서 1개 이상의 알파벳,숫자,특수문자를 포함해야 한다.")
 	private String password;
 
-	@NotNull(message = "SignUpRequest name notnull 에러")
 	@NotBlank(message = "SignUpRequest name notblank 에러")
 	private String name;
 
-	@NotNull(message = "SignUpRequest email notnull 에러")
-	@NotBlank(message = "SignUpRequest email notblank 에러")
-	private String email;
+	@NotBlank(message = "SignUpRequest nickname notblank 에러")
+	@Size(min = 2, message = "사용자 닉네임이 너무 짧습니다.")
+	@Pattern(regexp = "^[A-Za-z가-힣]+$", message = "닉네임은 한글 또는 알파벳만 입력해주세요.")
+	private String nickname;
 
-	@NotNull(message = "SignUpRequest address notnull 에러")
 	@NotBlank(message = "SignUpRequest address notblank 에러")
 	private String address;
 
 	@NotNull(message = "SignUpRequest joinedAt notnull 에러")
 	private LocalDate joinedAt;
-
-	@NotNull(message = "SignUpRequest nickname notnull 에러")
-	@NotBlank(message = "SignUpRequest nickname notblank 에러")
-	@Size(min = 2, message = "사용자 닉네임이 너무 짧습니다.")
-	@Pattern(regexp = "^[A-Za-z가-힣]+$", message = "닉네임은 한글 또는 알파벳만 입력해주세요.")
-	private String nickname;
 
 	private Integer mileage;
 
