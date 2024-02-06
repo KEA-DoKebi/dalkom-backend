fc89b8ce337bbe4e4ae71cc7cf5c2891e6b1bb64
Rhew Changmeen <fbckdals99@gmail.com>
2024-02-06 13:47:20 +0900

    [DOK-327] FAQ DTO 테스트 코드 작성 완료

--- src/main/java/com/dokebi/dalkom/domain/inquiry/dto/FaqReadOneResponse.java
+++ src/main/java/com/dokebi/dalkom/domain/inquiry/dto/FaqReadOneResponse.java
@@ -4,11 +4,13 @@ import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class FaqReadOneResponse {
 	private String title;
 	private LocalDateTime createdAt;
