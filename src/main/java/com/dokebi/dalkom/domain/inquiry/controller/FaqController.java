8fc43a5b43d2eeec1eca533b2416a745875b9e13
Rhew Changmeen <fbckdals99@gmail.com>
2024-02-06 11:42:11 +0900

    [DOK-327] test : FAQ CONTROLLER 테스트 코드 작성 완료

--- src/main/java/com/dokebi/dalkom/domain/inquiry/controller/FaqController.java
+++ src/main/java/com/dokebi/dalkom/domain/inquiry/controller/FaqController.java
@@ -25,7 +25,6 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 public class FaqController {
-
 	private final FaqService faqService;
 
 	// FAQ-001 (FAQ 상세 조회)
@@ -45,8 +44,7 @@ public class FaqController {
 	// FAQ-003 (FAQ 등록)
 	@PostMapping("/api/faq")
 	@ResponseStatus(HttpStatus.OK)
-	public Response createFaq(@LoginUser Long adminSeq,
-		@Valid @RequestBody FaqCreateRequest request) {
+	public Response createFaq(@LoginUser Long adminSeq, @Valid @RequestBody FaqCreateRequest request) {
 		faqService.createFaq(adminSeq, request);
 		return Response.success();
 	}
@@ -54,8 +52,7 @@ public class FaqController {
 	// FAQ-004 (FAQ 수정)
 	@PutMapping("/api/faq/{inquirySeq}")
 	@ResponseStatus(HttpStatus.OK)
-	public Response updateFaq(@LoginUser Long adminSeq,
-		@PathVariable Long inquirySeq,
+	public Response updateFaq(@LoginUser Long adminSeq, @PathVariable Long inquirySeq,
 		@Valid @RequestBody FaqUpdateRequest request) {
 		faqService.updateFaq(adminSeq, inquirySeq, request);
 		return Response.success();
@@ -69,7 +66,7 @@ public class FaqController {
 		return Response.success();
 	}
 
-	// FAQ-006 (FAQ 검색 )
+	// FAQ-006 (FAQ 검색)
 	@GetMapping("/api/faq/search")
 	@ResponseStatus(HttpStatus.OK)
 	public Response readFaqListBySearch(@RequestParam(required = false) String title, Pageable pageable) {
